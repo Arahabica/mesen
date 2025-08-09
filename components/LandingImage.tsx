04567e83af4e26bff83fd4bef73f83cd41e753fb
Arahabica <r.sasage@gmail.com>
2025-08-09 21:55:38 +0900

    トップページのアニメーション

--- components/LandingImage.tsx
+++ components/LandingImage.tsx
@@ -1,12 +1,21 @@
 import React, { useState, useEffect, useRef, useCallback } from 'react'
+import './LandingImage.css'
 
 
 export default function LandingImage() {
-  const [isLoaded, setIsLoaded] = useState(false)
-  const imgRef = useRef<HTMLImageElement>(null)
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [showMan, setShowMan] = useState(false);
+  const [showWoman, setShowWoman] = useState(false);
+  const imgRef = useRef<HTMLImageElement>(null);
 
   const onLoad = useCallback(() => {
-    setIsLoaded(true)
+    setIsLoaded(true);
+    if (Math.random() > 0.5) {
+      setShowMan(true);
+    }
+    if (Math.random() > 0.5) {
+      setShowWoman(true);
+    }
   }, []);
 
   useEffect(() => {
@@ -20,7 +29,7 @@ export default function LandingImage() {
   const imgClass = `w-full h-auto object-contain mb-8 ${isLoaded ? 'opacity-100' : 'opacity-0'}`;
 
   return (
-    <div style={{ width: '100%', position: 'relative' }}>
+    <div style={{ width: '100%', position: 'relative' }} className="landing-image-component">
       <img
         ref={imgRef}
         src="/mesen.webp"
@@ -49,28 +58,87 @@ export default function LandingImage() {
             y1="73"
             x2="110"
             y2="73"
-            stroke="#808080"
+            stroke="#666666"
             strokeWidth="20"
             strokeLinecap="round"
-            opacity="0"
-          >
-          <animate
-            attributeName="opacity"
-            from="0"
-            to="1"
-            dur="0.2s"
-            begin="0s"
-            fill="freeze"
           />
-          <animate
-            attributeName="x2"
-            from="65"
-            to="110"
-            dur="1s"
-            begin="0s"
-            fill="freeze"
+          <line
+            x1="300"
+            y1="50"
+            x2="345"
+            y2="50"
+            stroke="#666666"
+            strokeWidth="20"
+            strokeLinecap="round"
           />
-          </line>
+          <line
+            x1="465"
+            y1="45"
+            x2="500"
+            y2="45"
+            stroke="#666666"
+            strokeWidth="20"
+            strokeLinecap="round"
+          />
+          <line
+            x1="565"
+            y1="90"
+            x2="615"
+            y2="90"
+            stroke="#666666"
+            strokeWidth="20"
+            strokeLinecap="round"
+          />
+          <line
+            x1="660"
+            y1="47"
+            x2="690"
+            y2="47"
+            stroke="#666666"
+            strokeWidth="20"
+            strokeLinecap="round"
+          />
+          <line
+            x1="765"
+            y1="80"
+            x2="815"
+            y2="80"
+            stroke="#666666"
+            strokeWidth="20"
+            strokeLinecap="round"
+          />
+          <line
+            x1="910"
+            y1="59"
+            x2="960"
+            y2="59"
+            stroke="#666666"
+            strokeWidth="20"
+            strokeLinecap="round"
+          />
+          {showMan && (
+            <line
+
+              x1="280"
+              y1="445"
+              x2="410"
+              y2="445"
+              stroke="#666666"
+              strokeWidth="40"
+              strokeLinecap="round"
+            />
+          )}
+          {showWoman && (
+            <line
+              x1="590"
+              y1="520"
+              x2="710"
+              y2="520"
+              stroke="#666666"
+              strokeWidth="40"
+              strokeLinecap="round"
+            />
+          )}
         </svg>
       )}
     </div>
